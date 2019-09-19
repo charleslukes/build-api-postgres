38693947c9e228c76ae81805c2bac823bae787d1
Charles Chiakwa <charleslukes28@gmail.com>
2019-09-19 14:36:58 +0100

    add a feature to get a user and create users

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,35 @@ const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-const viewUser = async (req, res, next) => {
+const viewUsers = async (req, res, next) => {
   try {
     const result = await db.query("SELECT * FROM users");
     return res.json(result.rows);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const result = await db.query(
+      "SELECT * FROM users WHERE id=$1 RETURNING *",
+      [req.params.id]
+    );
+    return result;
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const createUsers = async (req, res, next) => {
+  try {
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    const result = await db.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *",
+      [req.body.username, hashPassword]
+    );
+    return res.json(result.rows[0]);
   } catch (error) {
     return next(error);
   }
