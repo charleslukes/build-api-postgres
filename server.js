24db2958818098895434af583ffa9b756bb940c8
Charles Chiakwa <charleslukes28@gmail.com>
2019-09-20 02:20:15 +0100

    add message to server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
 
+app.get("/", (req, res) => {
+  return res.json("Online now");
+});
 app.listen(process.env.PORT || 3004, () => {
   console.log("App Running on now >>>");
 });
