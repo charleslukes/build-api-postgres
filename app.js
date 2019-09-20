a758bf30ec8b2b0e5e7d027be4ff3c5bddfa25df
Charles Chiakwa <charleslukes28@gmail.com>
2019-09-20 02:46:09 +0100

    add cors check

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const cors = require("cors");
 require("dotenv").config();
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
